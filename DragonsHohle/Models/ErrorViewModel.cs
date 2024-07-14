d38a0caa494b2a12feabad2e31e4489d2f5fd336
Daniel <zhadowmgsff@gmail.com>
2024-07-14 11:03:01 -0600

    primer Commit

---
+++ DragonsHohle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DragonsHohle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
